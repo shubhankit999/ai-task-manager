42942bdeece4c176f306ac4392906e3f2b19e4f3
ankit_jain_bitbucket <ankit.jain@indusface.com>
2026-02-26 20:05:20 +0530

    changed folder

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./index.html', './**/*.{js,jsx}'],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
